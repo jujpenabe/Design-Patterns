317ee2604552c5fba67894442b8d93f6358c90dc
jujpenabe <jujpenabe@unal.edu.co>
2020-05-19 08:43:48 -0500

    Update CustomerPromoObserver.java

--- factory/CustomerPromoObserver.java
+++ factory/CustomerPromoObserver.java
@@ -1,1 +1,12 @@
 public class CustomerPromoObserver
+  implements Observer<Set<Promo>>{
+  private Subject<Set<Promo>> subject;
+  @Override
+  public void update( Set<Promo> promos ){
+    //Send message or notification with to the customer 
+  }
+  @Override public int hashCode( ){
+    //return a unique identifier
+  }
+}
+    
